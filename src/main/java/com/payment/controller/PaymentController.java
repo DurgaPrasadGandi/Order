2b6fd1fc56fefec0efd6e9e39a59018621ae3ce6
durgaprasadgandi27@gmail.com <durgaprasadgandi27@gmail.com>
2025-08-15 22:18:08 +0530

    Added Balance checkAPI

--- src/main/java/com/payment/controller/PaymentController.java
+++ src/main/java/com/payment/controller/PaymentController.java
@@ -1,8 +1,7 @@
 package com.payment.controller;
 
-import java.util.Random;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +22,10 @@ public class PaymentController {
     	
     }
     
+    @GetMapping("/checkBalance")
+    public String balanceCheck() {
+    	return "Sufficient Balance is available";
+    }
     
 	
 }
